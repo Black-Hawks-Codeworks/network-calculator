e42df25dc41c1285075b779f570d3497792d4d8d
Vasiliki <zarkavasi@gmail.com>
2025-12-29 22:48:11 +0200

    prospathw na kanw kati mhn to dwsete shmasia

--- src/modules/graph-placement/graph-placement-page.jsx
+++ src/modules/graph-placement/graph-placement-page.jsx
@@ -3,7 +3,8 @@ import styles from './graph-placement-page.module.css';
 export default function GraphPlacementPage() {
   return (
     <div className={styles.page}>
-      <h1>Graph Placement Page</h1>
+      <h1>Graph Layout</h1>
+      <div className={styles.graphContainer}></div>
     </div>
   );
 }
